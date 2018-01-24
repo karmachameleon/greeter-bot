cd057890281a1565dae5f43b68c65a5d8df4715c
Camille O'Leary <camilleoleary@bennington.edu>
2018-01-23 21:39:45 -0600

    added d20 dice roller

--- bot.js
+++ bot.js
@@ -371,6 +371,23 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         });
       break;
 
+      case 'ROLL':
+      case 'SKILLCHECK':
+        var diceroll = Math.floor(Math.random() * 21);
+        var diceresult = 'YOU HAVE ROLLED A + ' diceroll + ". "
+        if (diceroll == 1) { diceresult += 'AN ABJECT FAILURE. WHAT A SHAME'; }
+        else if (diceroll < 5) { diceresult += 'YOU HAVE ROLLED POORLY. BETTER LUCK NEXT TIME'; }
+        else if (diceroll < 9) { diceresult += 'A MEDIOCRE ROLL. PERHAPS IT WILL SUCCEED ON AN EASY TASK, LIKE WHAT AN ORGANIC BABY COULD DO'; }
+        else if (diceroll == 10) { diceresult += 'A COMPLETELY AVERAGE ROLL. ABOUT WHAT I EXPECTED'; }
+        else if (diceroll < 15) { diceresult += 'A DECENT ROLL. NOT BAD, BUT I EXPECT BETTER FROM YOU'; }
+        else if (diceroll < 19) { diceresult += 'HEY, THAT ROLL WAS ACTUALLYPRETTY GOOD'; }
+        else { diceresult += 'A CRITICAL SUCCESS! REMARKABLE'; }
+        bot.sendMessage({
+          to: channelID,
+          message: diceresult
+        })
+      break;
+
   	  case 'ROLE':
   		  if (message.split(' ').length == 1) {
           bot.sendMessage({
