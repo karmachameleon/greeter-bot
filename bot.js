46b0a00d9922ea34ee833ae69e955b05f22a05a2
Camille O'Leary <camilleoleary@bennington.edu>
2018-01-23 21:47:49 -0600

    the world is made of typos

--- bot.js
+++ bot.js
@@ -378,13 +378,13 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       case 'ROLL':
       case 'SKILLCHECK':
         var diceroll = Math.floor(Math.random() * 21);
-        var diceresult = 'YOU HAVE ROLLED A ' + diceroll.toString() + '. ';
+        var diceresult = 'YOU HAVE ROLLED ' + diceroll.toString() + '. ';
         if (diceroll == 1) { diceresult += 'AN ABJECT FAILURE. WHAT A SHAME'; }
         else if (diceroll < 5) { diceresult += 'YOU HAVE ROLLED POORLY. BETTER LUCK NEXT TIME'; }
         else if (diceroll <= 9) { diceresult += 'A MEDIOCRE ROLL. PERHAPS IT WILL SUCCEED ON AN EASY TASK, LIKE WHAT AN ORGANIC BABY COULD DO'; }
         else if (diceroll == 10) { diceresult += 'A COMPLETELY AVERAGE ROLL. ABOUT WHAT I EXPECTED'; }
         else if (diceroll <= 15) { diceresult += 'A DECENT ROLL. NOT BAD, BUT I EXPECT BETTER FROM YOU'; }
-        else if (diceroll <= 19) { diceresult += 'HEY, THAT ROLL WAS ACTUALLYPRETTY GOOD'; }
+        else if (diceroll <= 19) { diceresult += 'HEY, THAT ROLL WAS ACTUALLY PRETTY GOOD'; }
         else { diceresult += 'A CRITICAL SUCCESS! REMARKABLE'; }
         bot.sendMessage({
           to: channelID,
