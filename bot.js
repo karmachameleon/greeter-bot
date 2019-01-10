46d7254ee79e809c616b365a61a84ebc983a8380
Camille O'Leary <caramelkarmachameleon@gmail.com>
2019-01-09 19:45:57 -0600

    remove references to old auth.json

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 //var Discord = require('discord.io');
 var Discord = require('discord.js')
 var logger = require('winston');
-var auth = require('./auth.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
