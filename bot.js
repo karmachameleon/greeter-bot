4827d0d75d15074fc373837ba800ff738ebb09a3
Camille <caramelkarmachameleon@gmail.com>
2021-05-20 13:13:55 -0500

    capitalization

--- bot.js
+++ bot.js
@@ -1144,7 +1144,7 @@ bot.on('messageReactionAdd', (messageReaction, user) => {
 bot.on('messageReactionRemove', (messageReaction, user) => {
   if(messageReaction.emoji.name == '🍕'){
     if (messageReaction.me){
-      var newMessageFront = messageReaction.message.content.slice(0, messageReaction.message.content.lastindexOf(user.username) - 3);
+      var newMessageFront = messageReaction.message.content.slice(0, messageReaction.message.content.lastIndexOf(user.username) - 3);
       var newMessageBack = messageReaction.message.content.slice(messageReaction.message.content.lastIndexOf(user.username) + user.username.length);
       messageReaction.message.edit(newMessageFront + newMessageBack);
     }
