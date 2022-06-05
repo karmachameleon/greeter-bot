fffc8bf0044cbcf3051621bc12daeb6b768dd523
Camille <caramelkarmachameleon@gmail.com>
2022-06-05 15:55:31 -0400

    devictimized

--- commands/assimilate.js
+++ commands/assimilate.js
@@ -5,12 +5,11 @@ module.exports = {
     .setName('assimilate')
     .setDescription('Use this command to confirm you have read the rules of the Pizza Party and intend to abide by them.'),
   async execute(interaction) {
-    let newvictim = interaction.member;
-    if (!newvictim.roles.cache.has('538506263236182026')){
+    if (!interaction.member.roles.cache.has('538506263236182026')){
       await interaction.guild.roles.fetch('538506263236182026')
-      .then(role => newvictim.roles.add(role))
+      .then(role => interaction.member.roles.add(role))
       .then(interaction.guild.channels.cache.get("397067658052239361"))
-      .then(channel => channel.send('<@' + newvictim.id + '> DETECTED. YOU HAVE BEEN ASSIMILATED TO THE ROBOT PIZZA PARTY. POST AN INTRODUCTION IN <#396067802970193920>. OPTIONALLY, ADD YOUR PRONOUNS IN <#397067658052239361> (LOOK IN <#393842582414688269> FOR INSTRUCTIONS.) SHARE AND ENJOY :pizza:'))
+      .then(channel => channel.send('<@' + interaction.member.id + '> DETECTED. YOU HAVE BEEN ASSIMILATED TO THE ROBOT PIZZA PARTY. POST AN INTRODUCTION IN <#396067802970193920>. OPTIONALLY, ADD YOUR PRONOUNS IN <#397067658052239361> (LOOK IN <#393842582414688269> FOR INSTRUCTIONS.) SHARE AND ENJOY :pizza:'))
       .catch(console.error);
     }
     else {
