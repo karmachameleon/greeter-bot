6606933ed0f2d5f1c8122386c29f41e03f86da62
Camille <caramelkarmachameleon@gmail.com>
2022-06-05 17:10:34 -0400

    awaiting more patiently

--- commands/assimilate.js
+++ commands/assimilate.js
@@ -7,11 +7,13 @@ module.exports = {
   async execute(interaction) {
     //console.log(interaction.member.roles);
     //!interaction.member.roles.cache.has('538506263236182026')
-    if (true) {
+    const oldbie = await interaction.member.roles.cache.has('538506263236182026');
+    if (!oldbie) {
       //if (!interaction.member.roles.cache.has('538506263236182026')){
       await interaction.guild.roles.fetch('538506263236182026')
       .then(role => interaction.member.roles.add(role))
-      .then(interaction.guild.channels.fetch("397067658052239361"))
+      catch(console.error);
+      await(interaction.guild.channels.get("397067658052239361"))
       .then(channel => channel.send('<@' + interaction.member.id + '> DETECTED. YOU HAVE BEEN ASSIMILATED TO THE ROBOT PIZZA PARTY. POST AN INTRODUCTION IN <#396067802970193920>. OPTIONALLY, ADD YOUR PRONOUNS IN <#397067658052239361> (LOOK IN <#393842582414688269> FOR INSTRUCTIONS.) SHARE AND ENJOY :pizza:'))
       .catch(console.error);
     }
