ca8019d6a90489d6245c2d594a178aa762536972
Camille <caramelkarmachameleon@gmail.com>
2022-06-05 13:56:35 -0400

    is this allowed?

--- commands/assimilate.js
+++ commands/assimilate.js
@@ -6,15 +6,10 @@ module.exports = {
     .setDescription('Use this command to confirm you have read the rules of the Pizza Party and intend to abide by them.'),
   async execute(interaction) {
     let newvictim = interaction.member;
-    if (!newvictim.roles.cache.has(memberRole)){
-      await interaction.guild.roles.fetch('538506263236182026')
-      .then(role => newvictim.roles.add(role))
+    await interaction.guild.roles.fetch('538506263236182026')
+      .then(role => if (!newvictim.roles.cache.has(role)){ newvictim.roles.add(role) } else {return;})
       .then(interaction.guild.channels.cache.get("393845951292243980"))
       .then(channel => channel.send('<@' + newvictim.id + '> DETECTED. YOU HAVE BEEN ASSIMILATED TO THE ROBOT PIZZA PARTY. POST AN INTRODUCTION IN <#396067802970193920>. OPTIONALLY, ADD YOUR PRONOUNS IN <#397067658052239361> (LOOK IN <#393842582414688269> FOR INSTRUCTIONS.) SHARE AND ENJOY :pizza:'))
       .catch(console.error);
-    }
-    else {
-      return;
-    }
   },
 };
