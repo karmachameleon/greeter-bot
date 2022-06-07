ae9c808a39f1166dbef60b0ba1b37c7e9d961909
Camille <caramelkarmachameleon@gmail.com>
2022-06-07 07:59:20 -0400

    fixed duplicate name

--- commands/positive.js
+++ commands/positive.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('gender')
+    .setName('positive')
     .setDescription('A gentle reminder about the tone of this server.'),
   async execute(interaction) {
     await interaction.reply('PLEASE KEEP THIS PIZZA PARTY POSITIVE! YOU ARE WELCOME TO CONTINUE VENTING IN A PRIVATE CONVERSATION OR ANOTHER SPACE. THANK YOU')
