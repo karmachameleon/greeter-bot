7a11ebf64aeaf3a594310b9edb9b2c7a953e830f
Camille <caramelkarmachameleon@gmail.com>
2022-06-09 11:12:20 -0400

    beckon + birthday + jokes

---
+++ commands/beckon.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageAttachment } = require('discord.js');
+
+var cellarcontents = ["ROBOTS", "PIZZAS", "THULECITE ARTIFACTS", "TRINKETS", "LORE REVELATIONS", "NEW CHARACTERS", "SHIPWRECKED CHARACTERS", "EXOTIC BUTTERS", "CATBOYS", "CATS", "GOATS", "MIMES", "MACHINE OILS", "CHESTERS", "HAMBATS", "CHAOTIC CONSOLE COMMANDS", "MEMES", "BREADS", "LORE THEORIES", "SLIMES",
+"GMOD RAGDOLLS", "FUNNY VIDEOS", "FORTNITE DANCES", "FANCY RATS", "GENDERS", "STICKERS", "ANIMATIONS", "PICTURES OF WX-78", "ONCELERS", "FRUIT BASKETS"];
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('beckon')
+    .setDescription('HAL has something to show you in the basement, if you\'ll just come this way...'),
+  async execute(interaction) {
+    var temptation = cellarcontents[Math.floor(Math.random() * cellarcontents.length)];
+    var msgcont = "COME CLOSER. I HAVE MANY FINE " + temptation + " IN MY CELLAR. WOULD YOU LIKE TO HAVE A LOOK";
+    var beckonattach = new MessageAttachment('public/beckoningwxclaws.jpg');
+    var beckonembed = { image: { url: 'attachment://beckoningwxclaws.jpg', }, };
+    await interaction.reply({ content: msgcont, embeds: [fortembed], files: [fortattach] })
+    .catch(console.error);
+  },
+};
---
+++ commands/birthday.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('birthday')
+    .setDescription('Send a special birthday message.')
+    .addStringOption(option => option.setName('recipient').setDescription('The name of our beloved birthday pal.')).setRequired(true),
+  async execute(interaction) {
+    var name = interaction.options.getString('recipient');
+    await interaction.reply('HAPPY BIRTHDAY, PIZZA PARTIER! :birthday: :rat: :notes: CLICK HERE FOR A SPECIAL MESSAGE: https://itsyourbirthday.today/' + name))
+    .catch(console.error);
+  },
+};
