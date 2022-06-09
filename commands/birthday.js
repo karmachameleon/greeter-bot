6f776697f440aa639f821553ae5b5e30b6b26765
Camille <caramelkarmachameleon@gmail.com>
2022-06-09 11:17:14 -0400

    ) my beloathed

--- commands/birthday.js
+++ commands/birthday.js
@@ -4,7 +4,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('birthday')
     .setDescription('Send a special birthday message.')
-    .addStringOption(option => option.setName('recipient').setDescription('The name of our beloved birthday pal.')).setRequired(true),
+    .addStringOption(option => option.setName('recipient').setDescription('The name of our beloved birthday pal.').setRequired(true)),
   async execute(interaction) {
     var name = interaction.options.getString('recipient');
     await interaction.reply('HAPPY BIRTHDAY, PIZZA PARTIER! :birthday: :rat: :notes: CLICK HERE FOR A SPECIAL MESSAGE: https://itsyourbirthday.today/' + name)
